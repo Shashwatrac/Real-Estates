807a952b1858617fd4c4a5de1a8d01b07d91847a
Shashwatrac <rachitgupta326@gmail.com>
2026-02-18 19:55:22 +0530

    Initial clean commit

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Real Estate",
+    layout="wide",
+    page_icon="🏡"
+)
+
+st.title("A Place to Call HOME")
+
+# st.sidebar.header("About")
